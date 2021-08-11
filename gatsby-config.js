68dd9e5baeeb70967cd1df27d883fd1b8ac97494
enristra <enri99.es@gmail.com>
2021-08-11 15:15:06 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "Dentinelli",
+  },
+  plugins: [
+    "gatsby-plugin-netlify-cms",
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-sitemap",
+  ],
+};
